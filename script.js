7a539719107bdfef6789509e58eda8920b29213f
moonwalkie <moonwalkietalkie@gmail.com>
2023-07-01 00:45:09 +0200

    Add single round & edit computer random choice

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 // Game rules: rock beats scissors, scissors beat paper, and paper beats rock.
 
+const choices = ["Rock", "Paper", "Scissors"];
 
 // Function which randomly returns Rock, Paper or Scissors
 function getComputerChoice() {
-    let things = ["Rock", "Paper", "Scissors"];
-    let choice = things[Math.floor(Math.random()*things.length)];
-    return choice;
+    return choices[Math.floor(Math.random() * Math.floor(choices.length))]
 }
 
-console.log(getComputerChoice());
+// Function which plays a round and returns if you win or lose
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === choices[0]) && (computerSelection === choices[2])) {
+        return "You Win! Rock beats Scissors";
+    } else if ((playerSelection === choices[2]) && (computerSelection === choices[1])) {
+        return "You Win! Scissors beat Paper"; 
+    } else if ((playerSelection === choices[1]) && (computerSelection === choices[0])) {
+        return "You Win! Paper beats Rock";
+    } else if ((playerSelection === choices[2]) && (computerSelection === choices[0])) {
+        return "You Lose! Rock beats Scissors";
+    } else if ((playerSelection === choices[1]) && (computerSelection === choices[2])) {
+        return "You Lose! Scissors beat Paper";
+    } else if ((playerSelection == choices[0]) && (computerSelection === choices[1])) {
+        return "You Lose! Paper beats Rock";
+    } else {
+        return "Tie! Play another round";
+    }
+}
+
+const playerSelection = choices[0];
+const computerSelection = getComputerChoice();
+console.log("You chose: " + playerSelection);
+console.log("The computer has chosen: " + computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
