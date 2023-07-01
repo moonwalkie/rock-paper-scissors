6c93502e705dee826fb8eb89e61cab2de280e857
moonwalkie <moonwalkietalkie@gmail.com>
2023-07-01 17:24:28 +0200

    Add case-insensitive prompt to get user input

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * Math.floor(choices.length))]
 }
 
-// Function which plays a round and returns if you win or lose
+// Function which plays a round and returns win or lose
 function playRound(playerSelection, computerSelection) {
+    
     if ((playerSelection === choices[0]) && (computerSelection === choices[2])) {
         return "You Win! Rock beats Scissors";
     } else if ((playerSelection === choices[2]) && (computerSelection === choices[1])) {
@@ -26,7 +27,8 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = choices[0];
+let playerSelection = prompt("Make your pick (rock, paper or scissors): ", '');
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 const computerSelection = getComputerChoice();
 console.log("You chose: " + playerSelection);
 console.log("The computer has chosen: " + computerSelection);
